0ad6754c07113aba0e8828774b240237407a02cf
PhantomElixr <74986731+phantomelixr@users.noreply.github.com>
2022-10-30 19:53:08 -0600

    Delete assets directory

--- assets/js/neptuneGames.js
+++
@@ -1,8 +0,0 @@
-function updateState(state, update){
-    console.log('NeptuneGames | Updated gameState to '+state);
-    gameState = update;
-}
-
-function throwError(code){
-    console.log('NeptuneGames | An error occured: '+code);
-}
